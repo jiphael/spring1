039a400dc299ce87ca7bc07e8c688248b2be8730
jiphael <jiphael@gmail.com>
2020-09-02 13:37:47 +0900

    securitymvc 설정및 로그인초기

--- mvc/src/com/my/control/BoardRestController.java
+++ mvc/src/com/my/control/BoardRestController.java
@@ -1,133 +1,133 @@
-package com.my.control;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.my.exception.FindException;
-import com.my.model.PageBean;
-import com.my.service.BoardService;
-import com.my.vo.Board;
-
-@Controller
-@RequestMapping("/board/*")
-public class BoardRestController {
-	@Autowired
-	private BoardService service;
+////PACKAGE COM.MY.CONTROL;
+////
+////IMPORT ORG.SPRINGFRAMEWORK.BEANS.FACTORY.ANNOTATION.AUTOWIRED;
+////IMPORT ORG.SPRINGFRAMEWORK.HTTP.HTTPSTATUS;
+////IMPORT ORG.SPRINGFRAMEWORK.HTTP.RESPONSEENTITY;
+////IMPORT ORG.SPRINGFRAMEWORK.STEREOTYPE.CONTROLLER;
+////IMPORT ORG.SPRINGFRAMEWORK.WEB.BIND.ANNOTATION.PATHVARIABLE;
+////IMPORT ORG.SPRINGFRAMEWORK.WEB.BIND.ANNOTATION.REQUESTMAPPING;
+////IMPORT ORG.SPRINGFRAMEWORK.WEB.BIND.ANNOTATION.RESPONSEBODY;
+////
+////IMPORT COM.MY.EXCEPTION.FINDEXCEPTION;
+////IMPORT COM.MY.MODEL.PAGEBEAN;
+////IMPORT COM.MY.SERVICE.BOARDSERVICE;
+////IMPORT COM.MY.VO.BOARD;
+////
+////@CONTROLLER
+////@REQUESTMAPPING("/BOARD/*")
+////PUBLIC CLASS BOARDRESTCONTROLLER {
+////	@AUTOWIRED
+////	PRIVATE BOARDSERVICE SERVICE;
+////	
+////	@REQUESTMAPPING("/BOARDREST")
+////	PUBLIC VOID ROOT() {
+////		// / WEB-INF/JSP/OBARDREST.JSP로 포워드
+////		
+////	}
+//	@REQUESTMAPPING("/BOARDREST")
+//	PUBLIC STRING ROOT2() {
+//		RETURN "/BOARDREST"; //  WEB-INF/JSP/OBARDREST.JSP로 포워드
+//	}
 //	
-//	@RequestMapping("/boardRest")
-//	public void root() {
-//		// / web-inf/jsp/obardRest.jsp로 포워드
+//	@REQUESTMAPPING("/DEATIL/{BOARD_NO}")
+//	@RESPONSEBODY
+//	PUBLIC RESPONSEENTITY<BOARD> DETAIL(@PATHVARIABLE INT BOARD_NO){
+//			 BOARD BOARD =NULL;
+//		TRY {
+//			 BOARD = SERVICE.FINDBYNO(BOARD_NO);
+//			RETURN (RESPONSEENTITY<BOARD>)RESPONSEENTITY.STATUS(HTTPSTATUS.OK).BODY(BOARD);
+//		}CATCH(FINDEXCEPTION E) {
+//			E.PRINTSTACKTRACE();
+//			RETURN (RESPONSEENTITY<BOARD>)RESPONSEENTITY.STATUS(HTTPSTATUS.INTERNAL_SERVER_ERROR).BODY(BOARD);
+//		}
 //		
 //	}
-	@RequestMapping("/boardRest")
-	public String root2() {
-		return "/boardRest"; //  web-inf/jsp/obardRest.jsp로 포워드
-	}
-	
-	@RequestMapping("/deatil/{board_no}")
-	@ResponseBody
-	public ResponseEntity<Board> detail(@PathVariable int board_no){
-			 Board board =null;
-		try {
-			 board = service.findByNo(board_no);
-			return (ResponseEntity<Board>)ResponseEntity.status(HttpStatus.OK).body(board);
-		}catch(FindException e) {
-			e.printStackTrace();
-			return (ResponseEntity<Board>)ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(board);
-		}
-		
-	}
-	@RequestMapping("/list/{currentPage}")
-	@ResponseBody
-	public ResponseEntity<PageBean> list(@PathVariable(value="currentPage",required=false) Integer cp){
-		int currentPage = 1;
-		
-		if(cp !=null) {
-			currentPage = cp;
-		}
-		PageBean pb =null;
-		try {
-		pb = service.findAll(currentPage);
-		return (ResponseEntity<PageBean>)ResponseEntity.status(HttpStatus.OK).body(pb);
-	   }catch (FindException e) {
-					e.printStackTrace();
-					return (ResponseEntity<PageBean>)ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(pb);
-				}
-		
-	}
-
-//   	
-//   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-//	   String pathInfo = request.getPathInfo();
-//	   String servletPath = "";
-//	   if ("/write".equals(pathInfo)) {//글쓰기
-//		   String board_title =request.getParameter("board_title");
-//	      String board_writer = request.getParameter("board_writer");
-//	      String board_content = request.getParameter("board_content");
-//	      System.out.println(board_content);
-//	      Board board = new Board(board_title, board_writer, board_content);
-//	      try {
-//	         service.write(board);
-//	         servletPath = "/success.jsp";
-//	         RequestDispatcher rd = request.getRequestDispatcher(servletPath);
-//			 rd.forward(request, response);
-//	      } catch (AddException e) {
-//	         e.printStackTrace();
-//	      }
-//	   }else if("/replay".equals(pathInfo)) {//답글쓰기
-//		   
-//	   }else if("/detail".equals(pathInfo)) {//디테일보기
-//		  
-//		   
-//	   }
-//	   
-//	   else if("/list".equals(pathInfo)) {//게시물 목록보기
-//		   List<Board> list ;
-//		   try {
-//			   String strPage = request.getParameter("currentPage");
-//			   int currentPage = 1;
-//			   //if(strPage != null && !strPage.equals("")){
-//			   if(!"".equals(strPage)) { //if(!strPage.equals(""){
-//			   currentPage = Integer.parseInt(strPage);
-//			   }
-//			   com.my.model.PageBean pb = service.findAll(currentPage);
-//			   String url = request.getServletPath() + request.getPathInfo();
-//			   pb.setUrl(url);
-//			   System.out.println(pb);
-////			   list = service.findAll(currentPage);
-//			   //request.setAttribute("list", list);
-//			   request.setAttribute("pb", pb);
-//			   servletPath = "/boardList.jsp";
-//			   RequestDispatcher rd = request.getRequestDispatcher(servletPath);
-//			   rd.forward(request, response);
-////			   for(Board b:list) {
-////				   System.out.println(b); //b toString 자동호출
+//	@REQUESTMAPPING("/LIST/{CURRENTPAGE}")
+//	@RESPONSEBODY
+//	PUBLIC RESPONSEENTITY<PAGEBEAN> LIST(@PATHVARIABLE(VALUE="CURRENTPAGE",REQUIRED=FALSE) INTEGER CP){
+//		INT CURRENTPAGE = 1;
+//		
+//		IF(CP !=NULL) {
+//			CURRENTPAGE = CP;
+//		}
+//		PAGEBEAN PB =NULL;
+//		TRY {
+//		PB = SERVICE.FINDALL(CURRENTPAGE);
+//		RETURN (RESPONSEENTITY<PAGEBEAN>)RESPONSEENTITY.STATUS(HTTPSTATUS.OK).BODY(PB);
+//	   }CATCH (FINDEXCEPTION E) {
+//					E.PRINTSTACKTRACE();
+//					RETURN (RESPONSEENTITY<PAGEBEAN>)RESPONSEENTITY.STATUS(HTTPSTATUS.INTERNAL_SERVER_ERROR).BODY(PB);
+//				}
+//		
+//	}
+//
+////   	
+////   PROTECTED VOID SERVICE(HTTPSERVLETREQUEST REQUEST, HTTPSERVLETRESPONSE RESPONSE) THROWS SERVLETEXCEPTION, IOEXCEPTION {
+//
+////	   STRING PATHINFO = REQUEST.GETPATHINFO();
+////	   STRING SERVLETPATH = "";
+////	   IF ("/WRITE".EQUALS(PATHINFO)) {//글쓰기
+////		   STRING BOARD_TITLE =REQUEST.GETPARAMETER("BOARD_TITLE");
+////	      STRING BOARD_WRITER = REQUEST.GETPARAMETER("BOARD_WRITER");
+////	      STRING BOARD_CONTENT = REQUEST.GETPARAMETER("BOARD_CONTENT");
+////	      SYSTEM.OUT.PRINTLN(BOARD_CONTENT);
+////	      BOARD BOARD = NEW BOARD(BOARD_TITLE, BOARD_WRITER, BOARD_CONTENT);
+////	      TRY {
+////	         SERVICE.WRITE(BOARD);
+////	         SERVLETPATH = "/SUCCESS.JSP";
+////	         REQUESTDISPATCHER RD = REQUEST.GETREQUESTDISPATCHER(SERVLETPATH);
+////			 RD.FORWARD(REQUEST, RESPONSE);
+////	      } CATCH (ADDEXCEPTION E) {
+////	         E.PRINTSTACKTRACE();
+////	      }
+////	   }ELSE IF("/REPLAY".EQUALS(PATHINFO)) {//답글쓰기
+////		   
+////	   }ELSE IF("/DETAIL".EQUALS(PATHINFO)) {//디테일보기
+////		  
+////		   
+////	   }
+////	   
+////	   ELSE IF("/LIST".EQUALS(PATHINFO)) {//게시물 목록보기
+////		   LIST<BOARD> LIST ;
+////		   TRY {
+////			   STRING STRPAGE = REQUEST.GETPARAMETER("CURRENTPAGE");
+////			   INT CURRENTPAGE = 1;
+////			   //IF(STRPAGE != NULL && !STRPAGE.EQUALS("")){
+////			   IF(!"".EQUALS(STRPAGE)) { //IF(!STRPAGE.EQUALS(""){
+////			   CURRENTPAGE = INTEGER.PARSEINT(STRPAGE);
 ////			   }
-//		   }catch (FindException e) {
-//				   e.printStackTrace();
-//			   }
-//		   }   
-//	   }
-
-//글쓰기
-//	
-	// 답변보기
-//	  int Parent_no = 10;
-//	  String board_title = "테스트1";
-//      String board_writer = "작성자1";
-//      String board_content = "내용1";
-//      Board board = new Board(Parent_no,board_title, board_writer, board_content);
-//      try {
-//         service.reply(board);
-//      } catch (AddException e) {
-//         e.printStackTrace();
-//      }
-	//
-
-}
+////			   COM.MY.MODEL.PAGEBEAN PB = SERVICE.FINDALL(CURRENTPAGE);
+////			   STRING URL = REQUEST.GETSERVLETPATH() + REQUEST.GETPATHINFO();
+////			   PB.SETURL(URL);
+////			   SYSTEM.OUT.PRINTLN(PB);
+//////			   LIST = SERVICE.FINDALL(CURRENTPAGE);
+////			   //REQUEST.SETATTRIBUTE("LIST", LIST);
+////			   REQUEST.SETATTRIBUTE("PB", PB);
+////			   SERVLETPATH = "/BOARDLIST.JSP";
+////			   REQUESTDISPATCHER RD = REQUEST.GETREQUESTDISPATCHER(SERVLETPATH);
+////			   RD.FORWARD(REQUEST, RESPONSE);
+//////			   FOR(BOARD B:LIST) {
+//////				   SYSTEM.OUT.PRINTLN(B); //B TOSTRING 자동호출
+//////			   }
+////		   }CATCH (FINDEXCEPTION E) {
+////				   E.PRINTSTACKTRACE();
+////			   }
+////		   }   
+////	   }
+//
+////글쓰기
+////	
+//	// 답변보기
+////	  INT PARENT_NO = 10;
+////	  STRING BOARD_TITLE = "테스트1";
+////      STRING BOARD_WRITER = "작성자1";
+////      STRING BOARD_CONTENT = "내용1";
+////      BOARD BOARD = NEW BOARD(PARENT_NO,BOARD_TITLE, BOARD_WRITER, BOARD_CONTENT);
+////      TRY {
+////         SERVICE.REPLY(BOARD);
+////      } CATCH (ADDEXCEPTION E) {
+////         E.PRINTSTACKTRACE();
+////      }
+//	//
+//
+//}
